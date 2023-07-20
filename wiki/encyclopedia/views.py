32156026eb502f6e28883b74be846aec8d05e69c
Khaled Ahmed <105609031+kmohsen11@users.noreply.github.com>
2023-07-19 21:29:58 -0500

    Add files via upload

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render
+import markdown 
+from . import util
+import random
+def convert_to_html(entry):
+    content  = util.get_entry(entry)
+    markdowner = markdown.Markdown()
+    if content is None:
+        return None
+    else: 
+        return markdowner.convert(content)
+        
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    html_content = convert_to_html(title)
+    if html_content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "this page doesn't exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+
+def search(request):
+    if request.method =="POST":
+        entry_search = request.POST['q']
+        html_content = convert_to_html(entry_search) 
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+           } )
+        else:
+            all_entries = util.list_entries()
+            recommendations = []
+            for entry in all_entries:
+                if entry_search.lower() in entry.lower():
+                    recommendations.append(entry)
+                return render(request, "encyclopedia/search.html", {
+                    "recommendations": recommendations,})
+                    
+                    
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        if title in util.list_entries():
+            return render(request, "encyclopedia/error.html", {
+                "message": "this page already exists"
+            })
+        else:
+            util.save_entry(title, content)
+            html_content = convert_to_html(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content
+            })    
+def edit(request):
+    if request.method == "POST":
+        title = request.POST['entry_title']
+        content = util.get_entry(title) 
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
+        })
+    else:
+        title = request.GET['entry_title']
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
+        })
+def save_edit(request):
+    if request.method == "POST": 
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = convert_to_html(title)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+
+
+def rando(request):
+    all_entries = util.list_entries()
+    random_entry = random.choice(all_entries)
+    html_content = convert_to_html(random_entry)
+    return render(request, "encyclopedia/entry.html", {
+        "title": random_entry,
+        "content": html_content
+    })
